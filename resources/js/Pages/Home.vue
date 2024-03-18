d669654792095b8fe154d3e741ecaf606d74fc5f
vithyasagar-k <vithyasagarkarunanithi@gmail.com>
2024-03-18 17:03:14 +0530

    error fixed

--- resources/js/Pages/Home.vue
+++ resources/js/Pages/Home.vue
@@ -15,13 +15,6 @@ const stats = [
 
 <template>
 
-    <Head title="Dashboard" />
-
-    <AuthenticatedLayout>
-        <template #header>
-            <h2 class="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight">Dashboard</h2>
-        </template>
-
         <div class="py-12">
             <div class="max-w-3xl mx-auto sm:px-6 lg:px-8">
                 <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
@@ -137,5 +130,5 @@ const stats = [
             </div>
 
         </div>
-    </AuthenticatedLayout>
+ 
 </template>
